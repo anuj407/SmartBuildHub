bb0f3d7ebd1670dfb57972f83fba8fa2881256a7
arshad033 <arshadobra85@gmail.com>
2025-04-15 19:13:25 +0530

    feat: add lucide-react dependency, update App component structure, and include logo asset

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,15 +1,19 @@
-import React from 'react'
-import { Route, Routes } from 'react-router-dom'
-import Home from './Pages/Home'
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./Pages/Home";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-    <>
-     <Routes>
-      <Route path="/" element={<Home/>}/>
-     </Routes>
-    </>
-  )
+    <div className="">
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+      <Footer />
+    </div>
+  );
 }
 
-export default App
+export default App;
--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -1,16 +1,16 @@
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
 import { AppProvider } from "./context/AppContext.jsx";
-import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
 
-createRoot(document.getElementById('root')).render(
-    <React.StrictMode>
-        <AppProvider>
-            <BrowserRouter>
-              <App />
-            </BrowserRouter>
-        </AppProvider>
-    </React.StrictMode>
-)
+createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <AppProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </AppProvider>
+  </React.StrictMode>
+);
